6b96d1082b14a66b9a59de4553f491162decc7a1
Stefan Kurlyand <skurlyand7@gmail.com>
2020-04-27 18:25:34 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(417, 28);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(470, 397);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Введите начальную временную координату:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Введите начальную пространственную координату:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(306, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Введите начальную скорость (в единицах скорости света):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Введите начальное ускорение:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Укажите тип траектории:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 192);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(133, 20);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 231);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(133, 20);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Постоянная скорость",
+            "Постоянное ускорение"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(133, 21);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 456);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Диаграмма Пенроуза";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        double acc;
+        double v;
+        double xinit;
+        double tinit;
+        int choice;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double A(double x, double y1, double y2, double y3, double y4)
+            {
+                return y3;
+            }
+            double B(double x, double y1, double y2, double y3, double y4)
+            {
+                return y4;
+            }
+            double C(double x, double y1, double y2, double y3, double y4)
+            {
+                return acc * y4;
+            }
+            double D(double x, double y1, double y2, double y3, double y4)
+            {
+                return acc * y3;    
+            }
+            /* GL.Viewport(0, 0, Width, Height);
+
+             Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)(10*Math.PI / 180), 1f, 1f, 64f);
+             GL.MatrixMode(MatrixMode.Projection);
+             GL.LoadMatrix(ref projection);
+             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+             Matrix4 modelview = Matrix4.LookAt(0, 0, -15, 0, 0, 0, 0, 1, 0);
+             GL.MatrixMode(MatrixMode.Modelview);
+             GL.LoadMatrix(ref modelview);*/
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.ClearColor(0, 0, 0, 0);
+            GL.Color3(0f, 1f, 0f);
+            GL.Rotate(45f, 0.0f, 0.0f, 45.0f);
+            GL.LineWidth(1);
+            double a = 0.7;
+            double z = 0;
+       
+            GL.Begin(BeginMode.Lines);
+
+            GL.Vertex3(a, -a, z);
+            GL.Vertex3(-a, a, z);
+
+            GL.Vertex3(-a, a, z);
+            GL.Vertex3(-a, -a, z);
+
+            GL.Vertex3(-a, -a, z);
+            GL.Vertex3(a, -a, z);
+
+            GL.Vertex3(a, -a, z);
+            GL.Vertex3(a, a, z);
+
+            double p0, q0;
+            p0 = 2 * a * Math.Atan(-50) / 3.14;
+            q0 = 2 * a * Math.Atan(-50) / 3.14;
+            for (double j = -5; j <= 5; j += 0.25)
+            {
+                for (double i = -50; i < 50; i += 0.01)
+                {
+                    GL.Vertex3(p0, q0, z);
+                    p0 = 2 * a * Math.Atan(i + j) / 3.14;
+                    q0 = 2 * a * Math.Atan(i - j) / 3.14;
+                    if ((p0 > a) || (q0 > a) || (q0 < -a) || (p0 < -a)) break;
+                    GL.Vertex3(p0, q0, z);
+                }
+            }
+            for (double i = -5; i <= 5; i += 0.25)
+            {
+                for (double j = -50; j < 50; j += 0.01)
+                {
+                    GL.Vertex3(p0, q0, z);
+                    p0 = 2 * a * Math.Atan(i + j) / 3.14;
+                    q0 = 2 * a * Math.Atan(i - j) / 3.14;
+                    if ((p0 > a) || (q0 > a) || (q0 < -a) || (p0 < -a)) break;
+                    GL.Vertex3(p0, q0, z);
+                }
+            }
+
+            GL.End();
+
+            if (choice == 1)
+            {
+                GL.LineWidth(3);
+                GL.Begin(BeginMode.Lines);
+                GL.Color3(0.6, 0.3, 0.8);
+                double p, q;
+                p = 2 * a * Math.Atan(-(1 + v) * 50) / 3.14;
+                q = 2 * a * Math.Atan(-(1 - v) * 50) / 3.14;
+                for (double i = -50; i < 50; i += 0.01)
+                {
+                    GL.Vertex3(p, q, z);
+                    p = 2 * a * Math.Atan((1 + v) * i) / 3.14;
+                    q = 2 * a * Math.Atan((1 - v) * i) / 3.14;
+                    if ((p > a) || (q > a) || (q < -a) || (p < -a)) break;
+                    GL.Vertex3(p, q, z);
+                }
+                GL.End();
+            }
+
+
+            if (choice == 2)
+            {
+                GL.LineWidth(3);
+                GL.Begin(BeginMode.Lines);
+                GL.Color3(0.6, 0.3, 0.8);
+
+                double init = 0; double b = 100; double h = 0.001;
+                int n = (int)((b - init) / h);
+                int c = 4;
+                double[] X = new double[n];
+
+                double[,] F1 = new double[c, n];
+                double[,] F2 = new double[c, n];
+                double[,] F3 = new double[c, n];
+                double[,] F4 = new double[c, n];
+
+              
+                double[] Y1 = new double[n];
+                double[] Y2 = new double[n];
+                double[] Y3 = new double[n];
+                double[] Y4 = new double[n];
+
+
+     
+                int i = 0;
+                X[0] = init; Y1[0] = tinit; Y2[0] = xinit; Y3[0] = Math.Pow((1 - v * v), (-0.5)); Y4[0] = v * v / Math.Pow((1 - v * v), (-0.5));
+                for (int j = 1; j < n; j++)
+                {
+                    X[j] = init + j * h;
+
+                    F1[i,j] = h * A(X[j - 1], Y1[j - 1], Y2[j - 1], Y3[j - 1], Y4[j - 1]);
+                    F1[i + 1,j] = h * B(X[j - 1], Y1[j - 1], Y2[j - 1], Y3[j - 1], Y4[j - 1]);
+                    F1[i + 2,j] = h * C(X[j - 1], Y1[j - 1],- Y2[j - 1], Y3[j - 1], Y4[j - 1]);
+                    F1[i + 3,j] = h * D(X[j - 1], Y1[j - 1], Y2[j - 1], Y3[j - 1], Y4[j - 1]);
+
+
+                    F2[i,j] = h * A(X[j - 1] + h / 2.0, Y1[j - 1] + F1[i,j] / 2.0, Y2[j - 1] + F1[i +1 ,j] / 2.0, Y3[j - 1] + F1[i + 2,j] / 2.0, Y4[j - 1] + F1[i + 3,j] / 2.0);
+                    F2[i + 1,j] = h * B(X[j - 1] + h / 2.0, Y1[j - 1] + F1[i,j] / 2.0, Y2[j - 1] + F1[i + 1,j] / 2.0, Y3[j - 1] + F1[i + 2,j] / 2.0, Y4[j - 1] + F1[i + 3,j] / 2.0);
+                    F2[i + 2,j] = h * C(X[j - 1] + h / 2.0, Y1[j - 1] + F1[i,j] / 2.0, Y2[j - 1] + F1[i + 1,j] / 2.0, Y3[j - 1] + F1[i + 2,j] / 2.0, Y4[j - 1] + F1[i + 3,j] / 2.0);
+                    F2[i + 3,j] = h * D(X[j - 1] + h / 2.0, Y1[j - 1] + F1[i,j] / 2.0, Y2[j - 1] + F1[i + 1,j] / 2.0, Y3[j - 1] + F1[i + 2,j] / 2.0, Y4[j - 1] + F1[i + 3,j] / 2.0);
+
+                    F3[i,j] = h * A(X[j - 1] + h / 2.0, Y1[j - 1] + F2[i,j] / 2.0, Y2[j - 1] + F2[i + 1,j] / 2.0, Y3[j - 1] + F2[i + 2,j] / 2.0, Y4[j - 1] + F2[i + 3,j] / 2.0);
+                    F3[i + 1,j] = h * B(X[j - 1] + h / 2.0, Y1[j - 1] + F2[i,j] / 2.0, Y2[j - 1] + F2[i + 1,j] / 2.0, Y3[j - 1] + F2[i + 2,j] / 2.0, Y4[j - 1] + F2[i + 3,j] / 2.0);
+                    F3[i + 2,j] = h * C(X[j - 1] + h / 2.0, Y1[j - 1] + F2[i,j] / 2.0, Y2[j - 1] + F2[i + 1,j] / 2.0, Y3[j - 1] + F2[i + 2,j] / 2.0, Y4[j - 1] + F2[i + 3,j] / 2.0);
+                    F3[i + 3,j] = h * D(X[j - 1] + h / 2.0, Y1[j - 1] + F2[i,j] / 2.0, Y2[j - 1] + F2[i + 1,j] / 2.0, Y3[j - 1] + F2[i + 2,j] / 2.0, Y4[j - 1] + F2[i + 3,j] / 2.0);
+
+                    F4[i,j] = h * A(X[j - 1] + h, Y1[j - 1] + F3[i,j], Y2[j - 1] + F3[i + 1,j], Y3[j - 1] + F3[i + 2,j], Y4[j - 1] + F3[i + 3,j]);
+                    F4[i + 1,j] = h * B(X[j - 1] + h, Y1[j - 1] + F3[i,j], Y2[j - 1] + F3[i + 1,j], Y3[j - 1] + F3[i + 2,j], Y4[j - 1] + F3[i + 3,j]);
+                    F4[i + 2,j] = h * C(X[j - 1] + h, Y1[j - 1] + F3[i,j], Y2[j - 1] + F3[i + 1,j], Y3[j - 1] + F3[i + 2,j], Y4[j - 1] + F3[i + 3,j]);
+                    F4[i + 3,j] = h * D(X[j - 1] + h, Y1[j - 1] + F3[i,j], Y2[j - 1] + F3[i + 1,j], Y3[j - 1] + F3[i + 2,j], Y4[j - 1] + F3[i + 3,j]);
+
+                    Y1[j] = Y1[j - 1] + (F1[i,j] + 2 * F2[i,j] + 2 * F3[i,j] + F4[i,j]) / 6;
+                    Y2[j] = Y2[j - 1] + (F1[i + 1,j] + 2 * F2[i + 1,j] + 2 * F3[i + 1,j] + F4[i + 1,j]) / 6;
+                    Y3[j] = Y3[j - 1] + (F1[i + 2,j] + 2 * F2[i + 1,j] + 2 * F3[i + 2,j] + F4[i + 2,j]) / 6;
+                    Y4[j] = Y4[j - 1] + (F1[i + 3,j] + 2 * F2[i + 3,j] + 2 * F3[i + 3,j] + F4[i + 3,j]) / 6;
+                }
+                double p, q;
+                p = 2 * a *Math.Atan(Y1[0] + Y2[0]) / 3.14;
+                q = 2 * a * Math.Atan(Y1[0] - Y2[0]) / 3.14;
+                for (int k = 0; k < n; k++)
+                {
+                    GL.Vertex3(p, q, z);
+                    p = 2 * a * Math.Atan(Y1[k] + Y2[k]) / 3.14;
+                    q = 2 * a * Math.Atan(Y1[k] - Y2[k]) / 3.14;
+                    if ((p > a) || (q > a) || (q < -a) || (p < -a)) break;
+                    GL.Vertex3(p, q, z);
+                }
+                GL.End();
+            }
+
+                glControl1.SwapBuffers();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            xinit = Convert.ToDouble(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            tinit = Convert.ToDouble(textBox2.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            v = Convert.ToDouble(textBox3.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            acc = Convert.ToDouble(textBox4.Text);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                choice = 1;
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                choice = 2;
+            }
+        }
+    }
+}
